5ebe01c19efcca141f6dfa450d19f4124d9c5231
77Spencer77 <arunangshu20_ug@cse.nits.ac.in>
2022-04-04 21:01:55 +0530

    final changes done

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,3 +1,20 @@
+pragma solidity ^0.5.0;
+
 contract TodoList {
     uint256 public taskCount = 0;
+    struct Task {
+        uint256 id;
+        string content;
+        bool status;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out my github");
+    }
+
+    function createTask(string memory content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, content, false);
+    }
 }
